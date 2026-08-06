34d7b76ee2c2a59899cc8246e4e29709c4c98361
Arpan Poudel <1nx24cs021.arpan@nmit.ac.in>
2026-08-06 17:43:26 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (96.38%) - LeetSync

---
+++ ArrayEasy/1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.c
@@ -0,0 +1,28 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+bool* kidsWithCandies(int* candies, int candiesSize, int extraCandies, int* returnSize) {
+    int max=candies[0];
+    
+    
+    for(int i=0;i<candiesSize;i++)
+    {
+      if(candies[i]>max)
+      {
+        max=candies[i];
+      }
+    }
+    bool* result = malloc(candiesSize * sizeof(bool));
+
+    for(int i=0;i<candiesSize;i++)
+    {
+        result[i]=(candies[i]+extraCandies>=max);
+        
+    }
+
+    *returnSize=candiesSize;
+
+    return result;
+
+    
+}
