471c9ee9dea5fad1c6b0f13607757e1c54af773b
Arpan Poudel <1nx24cs021.arpan@nmit.ac.in>
2026-08-30 14:11:17 +0530

    Time: 0 ms (100.00%) | Memory: 23 MB (58.08%) - LeetSync

---
+++ BinarySearch/907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int low=1;
+        int high=*max_element(piles.begin(),piles.end());
+       int ans=INT_MAX;
+
+        while(low<=high)
+        {
+            long long mid=(low+high)/2;
+            long long totalhours=0;
+
+            for(int pile:piles)
+            {
+                totalhours+=(pile+mid-1)/mid;
+
+            }
+
+            if(totalhours<=h)
+            {
+                ans=mid;
+                high=mid-1;
+            }
+            else
+            {
+                low=mid+1;
+            }
+
+            
+
+        }
+        return ans;
+        
+    }
+};
