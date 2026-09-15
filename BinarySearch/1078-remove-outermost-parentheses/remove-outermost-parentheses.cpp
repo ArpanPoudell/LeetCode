13b5596e4ef871de1ac6a53d0c160d76c3bd92f8
Arpan Poudel <1nx24cs021.arpan@nmit.ac.in>
2026-09-15 21:50:24 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (54.21%) - LeetSync

---
+++ BinarySearch/1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+         string result = "";
+        int level = 0;
+
+        for(char ch : s)
+        {
+            if(ch == '(')
+            {
+                level++;
+
+                if(level > 1)
+                    result += ch;
+            }
+            else
+            {
+                level--;
+
+                if(level > 0)
+                    result += ch;
+            }
+        }
+
+        return result;
+        
+    }
+};
